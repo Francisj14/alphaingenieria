82fdad9d63834bff6267743cbb2394b804bb81e7
Francisj14 <75384294+Francisj14@users.noreply.github.com>
2024-03-05 20:45:16 -0500

    gallerytry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GalleryModule, GalleryConfig, ThumbnailsPosition } from 'ng-gallery';
+import { Observable, map } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 
 @Component({
+  template:`
+      <gallery *ngIf="galleryConfig$ | async; let config"
+              [items]="images$ | async"
+              [thumbWidth]="config.thumbWidth"
+              [thumbHeight]="config.thumbHeight"
+              [thumbPosition]="config.thumbPosition" />
+    `,
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-            HomeComponent],
+            HomeComponent,
+            GalleryModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'alpha';
+
+  galleryConfig$: Observable<GalleryConfig>;
+
+  constructor(breakpointObserver: BreakpointObserver) {
+
+    this.galleryConfig$ = breakpointObserver.observe([
+      Breakpoints.HandsetPortrait
+    ]).pipe(
+      map(res => {
+        if (res.matches) {
+          return {
+            thumbPosition: ThumbnailsPosition.Top,
+            thumbWidth: 80,
+            thumbHeight: 80
+          };
+        }
+        return {
+          thumbPosition: ThumbnailsPosition.Left,
+          thumbWidth: 120,
+          thumbHeight: 90
+        };
+      })
+    );
+  }
+
+  images: GalleryItem[];
+  
+  ngOnInit() {
+    this.images = [
+      new ImageItem({
+        src: 'IMAGE_SRC_URL',
+        thumb: 'IMAGE_THUMBNAIL_URL'
+      })
+  }
+
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { GalleryModule } from 'ng-gallery';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(GalleryModule)
+  ],
+  
 };
